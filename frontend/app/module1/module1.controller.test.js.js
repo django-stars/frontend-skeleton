2f03038849b0ce96087f712f38e34c42ea352f81
alexandervilko <vilkoalexander@gmail.com>
2015-09-11 18:42:09 +0300

    Renamed test file

--- frontend/app/module1/module1.controller.test.js.js
+++ frontend/app/module1/module1.controller.test.js.js
@@ -1,5 +1,5 @@
 require("angular-mocks");
-var module1 = require('module1/module1.controller');
+var Module1Controller = require('module1/module1.controller');
 
 describe('module1 controller test', function() {
     var ctrl,
@@ -7,7 +7,7 @@ describe('module1 controller test', function() {
 
     beforeEach(inject(function($rootScope, $controller) {
         scope = $rootScope.$new();
-        ctrl = $controller( module1, {$scope: scope});
+        ctrl = $controller( Module1Controller, {$scope: scope});
     }));
     it("Should be true", function() {
         expect(scope.greeting).toEqual('module 1 loaded');
