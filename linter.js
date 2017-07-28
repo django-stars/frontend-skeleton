0c24dee562dc60ebf8242edb8142facd14064722
SlavaB <vyacheslav.binetsky@gmail.com>
2017-07-28 18:01:28 +0300

    Add standard linter. Add possibility to modify standard config

---
+++ linter.js
@@ -0,0 +1,17 @@
+const eslint = require('eslint')
+const path = require('path')
+const pkg = require('./package.json')
+
+const opts = {
+  version: pkg.version,
+  homepage: pkg.homepage,
+  bugs: pkg.bugs.url,
+  eslint: eslint,
+  cmd: 'pocketlint',
+  eslintConfig: {
+    configFile: path.join(__dirname, '.eslintrc.json')
+  },
+  cwd: ''
+}
+
+require('standard-engine').cli(opts)
