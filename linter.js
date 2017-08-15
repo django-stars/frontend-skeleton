58b70149bd83278d5ca83d88fd4e7294f9a82717
SlavaB <vyacheslav.binetsky@gmail.com>
2017-08-15 19:08:48 +0300

    Update linter to react

--- linter.js
+++ linter.js
@@ -7,7 +7,7 @@ const opts = {
   homepage: pkg.homepage,
   bugs: pkg.bugs.url,
   eslint: eslint,
-  cmd: 'pocketlint',
+  cmd: 'linter',
   eslintConfig: {
     configFile: path.join(__dirname, '.eslintrc.json')
   },
