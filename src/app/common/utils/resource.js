f4633fb04cc58246c1a779dc0b0fedd614f627ef
Yaroslav Shulika <legendar@gmail.com>
2018-03-26 11:17:22 +0300

    resource update: options request, error handling, loading, list, uri build, uuid

--- src/app/common/utils/resource.js
+++ src/app/common/utils/resource.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
-import { compose } from 'redux'
+import { compose, bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { startSubmit, stopSubmit, setSubmitSucceeded, setSubmitFailed } from 'redux-form'
 
 import { fromPromise } from 'rxjs/observable/fromPromise'
 import { concat } from 'rxjs/observable/concat'
@@ -9,122 +10,211 @@ import { of } from 'rxjs/observable/of'
 
 import 'rxjs/add/operator/delay'
 import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/mergeMap'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/operator/filter'
+
+import pathToRegexp from 'path-to-regexp'
+
+import omit from 'lodash/omit'
+import merge from 'lodash/merge'
+import values from 'lodash/values'
 
 // TODO
-// OPTIONS request
-// errors handling
-// metadata handling
+// + OPTIONS request
+// + errors handling
+// +/- metadata handling (loading, global loading, etc)
 // global configuration
 // submit only dirty
-// isList
-// use uuid
-// endpoint generation (/name/:uuid, /name-:placeholder/)
+// caching (OPTIONS at least, also some resources can be cachable)
+// + isList
+// + use uuid
+// + endpoint generation (/name/:uuid, /name-:placeholder/)
 // pagination, filters, withRouter
 // debounce
 // List and Item components
 // HEAD request
 // custom: actions, reducer, epics
+// allowed methods
 
 const REQUEST = '@ds-resource/request'
 const SET_DATA = '@ds-resource/set-data'
+const SET_ERRORS = '@ds-resource/set-errors'
+const SET_LOADING = '@ds-resource/set-loading'
 
-export function request(payload, meta, resource = meta.resource) {
+export function request(payload, meta) {
   return {
     type: REQUEST,
-    meta: { ...meta, resource },
+    meta,
     payload,
   }
 }
 
-export function setData(payload, meta, resource = meta.resource) {
+export function setData(payload, meta) {
   return {
     type: SET_DATA,
-    meta: { ...meta, resource },
+    meta,
     payload,
   }
 }
 
-export function selectResource(resource, options) {
-  return function(state) {
-    return state.resource[resource.namespace] || { data: null }
+export function setErrors(payload, meta) {
+  return {
+    type: SET_ERRORS,
+    meta,
+    payload,
   }
 }
 
-export function selectFormResource(resource, options) {
-  return function(state) {
-    const resourceState = selectResource(resource, options)(state)
-    return {
-      ...resourceState,
-      initialValues: resourceState.data || {},
-    }
+export function setLoading(payload, meta) {
+  return {
+    type: SET_LOADING,
+    meta,
+    payload,
   }
 }
+
+
+export function selectResource(resource) {
+  return function(state) {
+    let resourceState = {
+      // FIXME wrong place for default state
+      data: null,
+      options: null,
+      isLoading: false,
+      errors: null,
+      loading: 0,
+      ...state.resource[resource.namespace],
+    }
+
+    return resourceState
+  }
+}
+
+
+// configuration:
+// 1. GLOBAL
+// 2. RESOURCE
+// 3. CONNECT PHASE
 
 export function connectResource(resource, options = {}) {
-  options = {
-    // FIXME global configuration
+  // assert(resource, 'no resource set') // TODO
+
+  resource = {
+    // TODO global configuration
+    // defaults
     idKey: 'uuid',
     prefetch: true,
+    refresh: false,
     form: false,
+    list: false,
+    options: false,
+    item: Boolean(options.form), // disallow binding list to form
+
+    ...resource,
     ...options,
   }
 
-  const { namespace } = resource
+  resource.namespace = getNamespace(resource)
 
   const connectHOC = connect(
     // data
-    options.form ? selectFormResource(resource) : selectResource(resource),
+    selectResource(resource),
     // actions
-    (dispatch) => ({
-      fetch: () => dispatch(request(undefined, { type: 'GET' }, resource)),
-      remove: () => dispatch(request(undefined, { type: 'DELETE' }, resource)),
-      save: (payload) => dispatch(request(payload, { type: 'PATCH' }, resource)),
-      update: (payload) => dispatch(request(payload, { type: 'PATCH' }, resource)),
-      create: (payload) => dispatch(request(payload, { type: 'POST' }, resource)),
-      replace: (payload) => dispatch(request(payload, { type: 'PUT' }, resource)),
+    (dispatch, props) => {
+      const meta = { resource, props }
 
-      // FIXME for form remove all other actions
-      // FIXME patch/post based on uuid
-      onSubmit: (payload) => dispatch(request(payload, { type: 'PATCH' }, resource)),
-    }),
+      let actions = {
+        create: makeRequestAction('POST', meta),
+        fetch: makeRequestAction('GET', meta),
+        update: makeRequestAction('PATCH', meta),
+        remove: makeRequestAction('DELETE', meta),
+        replace: makeRequestAction('PUT', meta),
+        fetchOptions: makeRequestAction('OPTIONS', meta),
+
+        setData: payload => setData(payload, meta),
+      }
+
+      actions = {
+        ...actions,
+        // aliases // TODO
+        save: actions.update,
+      }
+
+      return bindActionCreators(actions, dispatch)
+    },
+
     // merge
-    (stateProps, dispatchProps, ownProps) => ({
-      initialValues: stateProps.initialValues,
-      onSubmit: dispatchProps.onSubmit,
-      ...ownProps,
-      [namespace]: {
-        ...ownProps[namespace],
-        ...stateProps,
-        ...dispatchProps,
-      },
-    })
+    (stateProps, dispatchProps, ownProps) => {
+      let props = ({
+        [resource.namespace]: {
+          ...stateProps,
+          ...omit(dispatchProps, 'onSubmit'),
+        },
+      })
+
+      if(resource.form) {
+        props = {
+          ...ownProps,
+          ...props,
+          initialValues: stateProps.data,
+          onSubmit: (resource.list && ownProps[resource.idKey]) || (!resource.list && resource.prefetch)
+            ? dispatchProps.update : dispatchProps.create,
+        }
+      }
+
+      return props
+    },
   )
 
-  if(!options.prefetch) {
+  if(!resource.prefetch) {
     return connectHOC
   }
 
   return compose(
     connectHOC,
-    makePrefetchHOC(resource, options),
+    makePrefetchHOC(resource),
   )
 }
 
 export function connectFormResource(resource, options) {
-  return connectResource(resource, { ...options, form: true })
+  if(!options.form) {
+    // TODO assert
+    throw new Error('no form name. you must specify form name for connectFormResource')
+  }
+  return connectResource(resource, { ...options })
 }
 
 function makePrefetchHOC(resource) {
   return function(ComposedComponent) {
     return class PrefetchResourceContainer extends Component {
       componentDidMount() {
-        this.props[resource.namespace].fetch()
+        const hasData = this.props[resource.namespace].data !== null
+        const hasOptions = this.props[resource.namespace].options !== null
+        const hasId = Boolean(this.props[resource.idKey])
+
+        if((!hasData || resource.refresh) && (!resource.list || !resource.item || hasId)) {
+          // fetch item
+          this.props[resource.namespace].fetch()
+        } else if(!hasData) {
+          // register item
+          this.props[resource.namespace].setData({})
+        }
+
+
+        if(resource.options && !hasOptions) {
+          this.props[resource.namespace].fetchOptions()
+        }
       }
 
       render() {
-        if(this.props[resource.namespace].data === null) {
+        const hasData = this.props[resource.namespace].data !== null
+        const hasOptions = this.props[resource.namespace].options !== null
+
+        if(!hasData || (resource.options && !hasOptions)) {
           return null // TODO loading
         }
+
         return <ComposedComponent {...this.props} />
       }
     }
@@ -137,13 +227,43 @@ const defaultState = {
 
 export function reducer(state = defaultState, { type, payload = {}, meta = {}, error = false }) {
   switch (type) {
-    case SET_DATA:
+    case SET_DATA: {
+      const currentData = state[meta.resource.namespace]
+      const dataKey = {
+        [SET_DATA]: meta.type === 'OPTIONS' ? 'options' : 'data',
+        [SET_ERRORS]: 'errors',
+      }[type]
+
+      if(dataKey === 'options') {
+        payload = parseOptions(payload)
+      }
+
       return {
         ...state,
         [meta.resource.namespace]: {
-          data: payload,
+          ...currentData,
+          [dataKey]: payload,
         },
       }
+    }
+
+    case SET_LOADING: {
+      const currentData = state[meta.resource.namespace] || { loading: 0 }
+      const loading = currentData.loading + payload
+
+      if(loading < 0) {
+        console.warn('loading counter actions are inconsistent')
+      }
+
+      return {
+        ...state,
+        [meta.resource.namespace]: {
+          ...currentData,
+          isLoading: loading > 0,
+          loading,
+        },
+      }
+    }
   }
 
   return state
@@ -152,15 +272,52 @@ export function reducer(state = defaultState, { type, payload = {}, meta = {}, e
 export function epic(action$, store, { API }) { // FIXME API
   return action$.ofType(REQUEST)
     // .debounce(() => interval(100)) // FIXME: FAIL on different requests types
-    .switchMap(function({ meta, payload }) {
+    .mergeMap(function({ meta, payload }) {
+      const { type, props, resource } = meta
+      const toPath = pathToRegexp.compile(resource.endpoint)
+      const endpoint = toPath({ ...props, id: props[resource.idKey] })
+      const submitting = resource.form && ['POST', 'PATCH', 'PUT', 'DELETE'].includes(type)
+
       return concat(
-        // of(actions.setLoading(true)),
-        fromPromise(API(meta.resource.endpoint).request(meta.type, {}, payload))
+        of(
+          setLoading(+1, meta),
+          submitting && startSubmit(resource.form),
+        ),
+        fromPromise(API(endpoint).request(type, {}, payload))
           .switchMap(response => of(
             setData(response, meta),
-            // actions.setLoading(false),
+            setLoading(-1, meta),
+            submitting && stopSubmit(resource.form),
+            submitting && setSubmitSucceeded(resource.form),
           ))
-          // .catch(err => of(actions.setErrors(err)))
-      )
+          .catch(err => of(
+            setErrors(err.errors || err, meta),
+            setLoading(-1, meta),
+            submitting && stopSubmit(resource.form, err.errors || err),
+            submitting && setSubmitFailed(resource.form), // TODO fields list
+          ))
+      ).filter(Boolean)
     })
+}
+
+function getNamespace({ list, item, namespace }) {
+  if(!Array.isArray(namespace)) {
+    namespace = [namespace, namespace]
+  }
+
+  return namespace[list && !item ? 0 : 1]
+}
+
+function makeRequestAction(type, meta) {
+  return function(payload) {
+    if(type === 'GET' && payload !== undefined) {
+      // TODO assert here
+      console.warn('GET action should not contain request body')
+    }
+    return request(payload, { ...meta, type })
+  }
+}
+
+function parseOptions(options) {
+  return merge.apply(null, values(options.actions))
 }
