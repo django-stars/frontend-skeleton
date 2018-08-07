bb6af7b2594949ae88a1b2994427ae84b427b480
Yaroslav Shulika <legendar@gmail.com>
2018-08-07 18:48:58 +0300

    allow to expand env variables

--- init-env.js
+++ init-env.js
@@ -1,34 +1,20 @@
 import dotenv from 'dotenv'
+import dotenvExpand from 'dotenv-expand'
 
-dotenv.config({
+
+// load .env file
+const config = dotenv.config({
   // dotenv use .env by default, but you can override this
   path: process.env.ENVFILE,
 })
 
-// load default config from .env.example
-dotenv.config({
-  // dotenv use .env by default, but you can override this
+// load default config from .env.default
+const configDefault = dotenv.config({
   path: '.env.default',
 })
 
-/*
-const defaults = {
-  BACKEND_URL: 'http://localhost:8000',
-  API_URL: '/api/v1/',
-  SOURCES_PATH: 'src',
-  OUTPUT_PATH: 'dist',
-  PUBLIC_PATH: 'assets',
-  DEV_SERVER_PORT: 3000,
-  AUTH_HEADER: 'Authorization',
-  PROXY: ['/static/', '/media/'],
-  ENABLE_META: false,
-}
-
-Object.keys(defaults).forEach(function (key) {
-  if (!process.env.hasOwnProperty(key)) {
-    process.env[key] = defaults[key]
-  }
-})
-*/
+// expand variables
+dotenvExpand(config)
+dotenvExpand(configDefault)
 
 export default process.env
