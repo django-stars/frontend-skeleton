99247bb05ef3390eb97402da2ea2fc6b3866828c
Yaroslav Shulika <legendar@gmail.com>
2018-08-08 19:14:56 +0300

    proxy and spa/mpa rewrite: support subdomain, SSR option, remove lot of code duplication

--- presets/index.js
+++ presets/index.js
@@ -1,18 +1,18 @@
 import babel from './babel'
-import mpa from './mpa'
 import postcss from './postcss'
 import react from './react'
 import sass from './sass'
 import spa from './spa'
 import assets from './assets'
+import proxy from './proxy'
 
 
 export {
   babel,
-  mpa,
   postcss,
   react,
   sass,
   spa,
   assets,
+  proxy,
 }
