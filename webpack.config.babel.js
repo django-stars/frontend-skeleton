676e02baf24b14c0402eeac7775046093ac824ed
Sasha <aleksandr.gulchenko@gmail.com>
2019-09-09 15:16:02 +0300

    add stylelint

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,8 +3,6 @@ import './init-env' // SHOULD BE FIRST
 import path from 'path'
 import CleanWebpackPlugin from 'clean-webpack-plugin'
 import WriteFilePlugin from 'write-file-webpack-plugin'
-import SentryWebpackPlugin from '@sentry/webpack-plugin'
-
 // import ReloadPlugin from 'reload-html-webpack-plugin'
 
 import {
@@ -93,19 +91,6 @@ module.exports = createConfig([
       },
     },
   }),
-
-
-  env('production', [
-    addPlugins([
-      new SentryWebpackPlugin({
-        include: '.',
-        ignoreFile: '.sentrycliignore',
-        ignore: ['node_modules', 'webpack.config.babel.js', 'presets'],
-        configFile: 'sentry.properties',
-      }),
-    ]),
-  ]),
-
 
   env('development', [
     devServer({
