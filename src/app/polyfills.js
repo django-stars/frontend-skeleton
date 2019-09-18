a94c476b1367902785b3b9f3eb5a75b062d1faa1
alexgul4enko <aleksandr.gulchenko@gmail.com>
2019-09-18 17:07:10 +0300

    polyfills
    * update polyfills usage
    * remove babel-polyfill

--- src/app/polyfills.js
+++ src/app/polyfills.js
@@ -1,11 +1,9 @@
-import 'babel-polyfill'
 import smoothScroll from 'smoothscroll-polyfill'
 smoothScroll.polyfill()
 
 // should be after React import for IE11
 // 'require' used because inside condition
 if(!!window.MSInputMethodContext && !!document.documentMode) { // IE11 check
-  require('core-js/modules/es6.symbol')
   require('whatwg-fetch')
 
   // classList.toggle polyfill for IE
