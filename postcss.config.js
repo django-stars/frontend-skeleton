f484b10dd36eeec18b18d03b2685e4496416071a
Nike Mazur <nike@Nikes-MacBook-Pro.local>
2020-02-14 15:01:47 +0200

    Add fix to autoprefixer regarded terminal error

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
   sourceMap: true,
   plugins: [
     autoprefixer({
-      browsers: ['last 2 versions'],
+      overrideBrowserslist: ['last 2 versions'],
     }),
     // TODO svg optimizations https://github.com/TrySound/postcss-inline-svg#how-to-optimize-svg-on-build-step
     inlineSVG({
