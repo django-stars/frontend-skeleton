0e04dec64c11bc7a54b9fffda493501c7214c4c0
Yaroslav Shulika <legendar@gmail.com>
2020-02-14 15:12:58 +0200

    provide comment

--- presets/styles.js
+++ presets/styles.js
@@ -10,6 +10,11 @@ export default function(config) {
         },
       }),
     ]),
+    // NOTE we can't use path.resolve for exclude
+    // path.resolve doesn't resolve symlinks
+    // in docker containers node_modules folder usually placed in separate symlinked directories
+    // path.resolve will provide incorrect string, so we need to use RegExp here
+    // more documentation here: https://webpack.js.org/configuration/module/#condition
     match(['*.css', '*.sass', '*.scss'], { exclude: /node_modules/ }, [
       process.env.SSR ? css() : css.modules({ camelCase: true }),
       sass({
