9e0d3cc836fa8f85e6804d165610ed78c1280b81
Sasha <aleksandr.gulchenko@gmail.com>
2020-02-26 14:18:20 +0200

    fix validation example

--- src/app/common/forms/validation/email.js
+++ src/app/common/forms/validation/email.js
@@ -1,11 +1,6 @@
-import { mainValidation } from './utils'
 import errors from './constants'
 
-export function validateEmail(fields) {
-  return mainValidation(fields, email)
-}
-
-export function email(value) {
+export default function email(value) {
   const emailRe = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\u0080-\u00ff\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return (value && !emailRe.test(value)) ? errors.email : undefined
 }
--- src/app/common/forms/validation/required.js
+++ src/app/common/forms/validation/required.js
@@ -1,10 +1,5 @@
-import { mainValidation } from './utils'
 import errors from './constants'
 
-export function validateRequired(fields) {
-  return mainValidation(fields, required)
-}
-
-export function required(value) {
+export default function required(value) {
   return !value ? errors.required : undefined
 }
