ff51ddb5f758688d6c26ff9344896c72d4bf1fc2
Sasha <aleksandr.gulchenko@gmail.com>
2020-04-22 12:11:02 +0300

    fix babel configs

--- babel.config.js
+++ babel.config.js
@@ -1,37 +1,33 @@
-{
+module.exports = {
   'compact': false,
   'presets': [[
-    '@babel/preset-env', 
+    '@babel/preset-env',
     {
       'useBuiltIns': 'usage',
       'corejs': {
-        version: 3,
-        proposals: true
-      }
-    }
+        'version': 3,
+        'proposals': true,
+      },
+    },
   ]],
   'plugins': [
-    // stage 2
+  // stage 2
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-    // '@babel/plugin-proposal-function-sent',
     '@babel/plugin-proposal-numeric-separator',
     '@babel/plugin-proposal-throw-expressions',
-
     // stage 3
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
     ['@babel/plugin-proposal-class-properties', { 'loose': false }],
     '@babel/plugin-proposal-json-strings',
     '@babel/plugin-proposal-export-namespace-from',
-
   ],
-
   'env': {
     'test': {
       'presets': ['@babel/preset-react', '@babel/preset-flow'],
       'plugins': [
         'babel-plugin-react-require',
       ],
-    }
-  }
+    },
+  },
 }
