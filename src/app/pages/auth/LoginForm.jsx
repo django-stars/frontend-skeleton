e9fbe46f11394d9a9851abe51824460106bb3701
Dudeson <Dudeson26@gmail.com>
2020-08-19 17:01:23 +0300

    Icon example & login form styles

--- src/app/pages/auth/LoginForm.jsx
+++ src/app/pages/auth/LoginForm.jsx
@@ -1,12 +1,15 @@
 import { TextField } from 'common/forms'
+import styles from './loginForm.scss'
 
 export default function LoginForm({ handleSubmit }) {
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
-      <TextField name="email" label="Email" type="email" />
-      <TextField name="password" label="Password" type="password" />
-      <button>Login</button>
-    </form>
+    <div className={styles.loginContainer}>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <TextField name="email" label="Email" type="email" />
+        <TextField name="password" label="Password" type="password" />
+        <button className={styles.loginButton}> <i className={styles.keyIcon} /> Login</button>
+      </form>
+    </div>
   )
 }
