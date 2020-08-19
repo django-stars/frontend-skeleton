c12a7647b6c71d3c41ba27fabbcd91e2503f8140
Dudeson <Dudeson26@gmail.com>
2020-08-19 13:49:43 +0300

    Image import example
    Conflicts:
    	src/app/layouts/Header.jsx

--- src/app/layouts/Header.jsx
+++ src/app/layouts/Header.jsx
@@ -1,11 +1,12 @@
 import { Component } from 'react'
 import { Link } from 'common/router/Link'
+import logo from '../../img/ds-logo.png'
 
 export default class Header extends Component {
   render() {
     return (
       <header>
-        <Link to="root">Logo</Link>
+        <Link to="root"><img src={logo} alt='logo'/></Link>
       </header>
     )
   }
