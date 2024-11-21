3ca63c8bb384c0a80cb511b241ac2e89936a7791
Yaroslav Shulika <legendar@gmail.com>
2024-11-21 14:00:29 +0200

    upgrade babel group

--- babel.config.js
+++ babel.config.js
@@ -14,14 +14,14 @@ module.exports = {
   'plugins': [
   // stage 2
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-transform-numeric-separator',
     '@babel/plugin-proposal-throw-expressions',
     // stage 3
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
-    ['@babel/plugin-proposal-class-properties', { 'loose': false }],
-    '@babel/plugin-proposal-json-strings',
-    '@babel/plugin-proposal-export-namespace-from',
+    ['@babel/plugin-transform-class-properties', { 'loose': false }],
+    '@babel/plugin-transform-json-strings',
+    '@babel/plugin-transform-export-namespace-from',
   ],
   'env': {
     'test': {
